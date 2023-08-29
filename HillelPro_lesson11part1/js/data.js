4fb617495f7c3c3d6217006a3657df25c63a5a29
Anton Anikonov <antonanikonov1@gmail.com>
2023-08-29 11:41:22 +0300

    re add files

---
+++ HillelPro_lesson11part1/js/data.js
@@ -0,0 +1,65 @@
+const categories = {
+  books: {
+    name: 'Books',
+    products: [
+      {
+        id: 1,
+        name: 'Kindle',
+        description: 'Kindle book .... description',
+        price: 100,
+      },
+      {
+        id: 2,
+        name: 'Pocketbook',
+        description: 'Pocketbook - book .... description',
+        price: 150,
+      },
+    ],
+  },
+  phones: {
+    name: 'Phones',
+    products: [
+      {
+        id: 3,
+        name: 'Samsung Galaxy S23 Ultra',
+        price: 2000,
+        description: 'galaxy ... description'
+      },
+      {
+        id: 4,
+        name: 'iPhone 100',
+        price: 1500,
+        description: 'iphone is not just a phone'
+      },
+      {
+        id: 5,
+        name: 'Huawei',
+        price: 1000,
+        description: 'china-phone'
+      }
+    ],
+  },
+  tablets: {
+    name: 'Tablets',
+    products: [
+      {
+        id: 6,
+        name: 'iPad',
+        price: 1000,
+        description: '...description...'
+      },
+      {
+        id: 7,
+        name: 'iPad Pro',
+        price: 1500,
+        description: '...description...'
+      },
+      {
+        id: 8,
+        name: 'Samsung Tab X',
+        price: 1700,
+        description: '...description...'
+      },
+    ],
+  }
+};
