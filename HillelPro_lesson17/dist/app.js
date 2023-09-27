5f15482bf1b34c7040918dc1fca6dfdc607e732e
Anton Anikonov <antonanikonov1@gmail.com>
2023-09-27 12:08:17 +0300

    first commit

---
+++ HillelPro_lesson17/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var myFunc = function myFunc(a, b, c) {
+  return a + b + c;
+};
+myFunc(3, 5, 7);
+var variable = true;
---
+++ HillelPro_lesson18/js/functions.js
@@ -0,0 +1,52 @@
+const initSlider = () => {
+    const imgWrapper = createElement(`img`,`#sliderWrapper`,``,{alt:'image',id:'imageWrapper'});
+    imgWrapper.src = `./img/` + `${images[imgCount]}`;
+}
+const prevBtn =()=>{
+    createElement(`a`,'#sliderWrapper',``,{id:'prevBtn',href:'#'},{click:(event)=>{
+            event.preventDefault();
+            imgCount = imgCount <= 0 ? images.length - 1 : imgCount - 1;
+            document.getElementById('imageWrapper').src = `./img/`+ images[imgCount];
+        }})
+    createElement(`img`,`#prevBtn`,``,{id:'leftImg',src:'../img/svg/left.svg'});
+}
+const nextBtn = () =>{
+    createElement(`a`,'#sliderWrapper','',{id:`nextBtn`,href:'#'},{click:(event)=>{
+            event.preventDefault();
+            imgCount = imgCount === images.length - 1 ? 0 : imgCount + 1;
+            document.getElementById('imageWrapper').src = `./img/`+ images[imgCount];
+        }})
+    createElement(`img`,`#nextBtn`,``,{id:'rightImg',src:'../img/svg/right.svg',});
+}
+
+const autoPlay = () =>{
+    let interval;
+    createElement(`a`,`#main`,'',{id:'autoPlayBtn',href:'#'},{click:(event)=>{
+        event.preventDefault();
+        interval = toggleAutoPlay(interval);
+        toggleImage()
+        }})
+    createElement(`img`,`#autoPlayBtn`,``,{id:'autoImg',src:`../img/svg/play.svg`})
+}
+const toggleAutoPlay = (interval) =>{
+    if (interval){
+        clearInterval(interval);
+        interval=null;
+    }else{
+        interval = setInterval(() => {
+            imgCount = imgCount === images.length - 1 ? 0 : imgCount + 1;
+            // If U need reverse
+            // imgCount = imgCount === 0 ? images.length - 1 : imgCount - 1;
+            document.getElementById(`imageWrapper`).src = `./img/` + images[imgCount];
+        }, 3000)
+    }
+    return interval
+}
+const toggleImage=()=>{
+    const autoImg = document.getElementById(`autoImg`)
+    if (autoImg.src.includes('play')){
+        document.getElementById(`autoImg`).src=`../img/svg/pause.svg`;
+    }else{
+        document.getElementById(`autoImg`).src=`../img/svg/play.svg`;
+    }
+}
