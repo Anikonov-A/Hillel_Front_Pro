0128b547d7cafa9193501c6dc3d1071649d61480
Anton Anikonov <antonanikonov1@gmail.com>
2023-09-27 13:07:57 +0300

    fix issues

--- HillelPro_lesson18/js/functions.js
+++ HillelPro_lesson18/js/functions.js
@@ -1,38 +1,53 @@
+let interval;
+
 const initSlider = () => {
-    const imgWrapper = createElement(`img`,`#sliderWrapper`,``,{alt:'image',id:'imageWrapper'});
+    const imgWrapper = createElement(`img`, `#sliderWrapper`, ``, {alt: 'image', id: 'imageWrapper'});
     imgWrapper.src = `./img/` + `${images[imgCount]}`;
 }
-const prevBtn =()=>{
-    createElement(`a`,'#sliderWrapper',``,{id:'prevBtn',href:'#'},{click:(event)=>{
+const prevBtn = () => {
+    createElement(`a`, '#sliderWrapper', ``, {id: 'prevBtn', href: '#'}, {
+        click: (event) => {
             event.preventDefault();
             imgCount = imgCount <= 0 ? images.length - 1 : imgCount - 1;
-            document.getElementById('imageWrapper').src = `./img/`+ images[imgCount];
-        }})
-    createElement(`img`,`#prevBtn`,``,{id:'leftImg',src:'./img/svg/left.svg'});
+            document.getElementById('imageWrapper').src = `./img/` + images[imgCount];
+            if (interval) {
+                interval = clearedInterval(interval)
+                interval = toggleAutoPlay(interval);
+            }
+
+        }
+    })
+    createElement(`img`, `#prevBtn`, ``, {id: 'leftImg', src: './img/svg/left.svg'});
 }
-const nextBtn = () =>{
-    createElement(`a`,'#sliderWrapper','',{id:`nextBtn`,href:'#'},{click:(event)=>{
+const nextBtn = () => {
+    createElement(`a`, '#sliderWrapper', '', {id: `nextBtn`, href: '#'}, {
+        click: (event) => {
             event.preventDefault();
             imgCount = imgCount === images.length - 1 ? 0 : imgCount + 1;
-            document.getElementById('imageWrapper').src = `./img/`+ images[imgCount];
-        }})
-    createElement(`img`,`#nextBtn`,``,{id:'rightImg',src:'./img/svg/right.svg',});
+            document.getElementById('imageWrapper').src = `./img/` + images[imgCount];
+            if (interval) {
+                interval = clearedInterval(interval)
+                interval = toggleAutoPlay(interval);
+            }
+        }
+    })
+    createElement(`img`, `#nextBtn`, ``, {id: 'rightImg', src: './img/svg/right.svg',});
 }
 
-const autoPlay = () =>{
-    let interval;
-    createElement(`a`,`#main`,'',{id:'autoPlayBtn',href:'#'},{click:(event)=>{
-        event.preventDefault();
-        interval = toggleAutoPlay(interval);
-        toggleImage()
-        }})
-    createElement(`img`,`#autoPlayBtn`,``,{id:'autoImg',src:`./img/svg/play.svg`})
+const autoPlay = () => {
+    createElement(`a`, `#main`, '', {id: 'autoPlayBtn', href: '#'}, {
+        click: (event) => {
+            event.preventDefault();
+            interval = toggleAutoPlay(interval);
+            toggleImage()
+        }
+    })
+    createElement(`img`, `#autoPlayBtn`, ``, {id: 'autoImg', src: `./img/svg/play.svg`})
 }
-const toggleAutoPlay = (interval) =>{
-    if (interval){
-        clearInterval(interval);
-        interval=null;
-    }else{
+const toggleAutoPlay = (interval) => {
+    if (interval) {
+       interval = clearedInterval(interval)
+    } else {
         interval = setInterval(() => {
             imgCount = imgCount === images.length - 1 ? 0 : imgCount + 1;
             // If U need reverse
@@ -42,11 +57,16 @@ const toggleAutoPlay = (interval) =>{
     }
     return interval
 }
-const toggleImage=()=>{
+const toggleImage = () => {
     const autoImg = document.getElementById(`autoImg`)
-    if (autoImg.src.includes('play')){
-        document.getElementById(`autoImg`).src=`./img/svg/pause.svg`;
-    }else{
-        document.getElementById(`autoImg`).src=`./img/svg/play.svg`;
+    if (autoImg.src.includes('play')) {
+        document.getElementById(`autoImg`).src = `./img/svg/pause.svg`;
+    } else {
+        document.getElementById(`autoImg`).src = `./img/svg/play.svg`;
     }
+}
+const clearedInterval = (interval) => {
+    clearInterval(interval);
+    interval = null;
+    return interval;
 }
