9c963b19fda0e3bbeb79d33957cc098f0abf2f91
Anton Anikonov <antonanikonov1@gmail.com>
2023-09-29 17:26:52 +0300

    added buttons and connected to swapi

--- lesson_19/js/app.js
+++ lesson_19/js/app.js
@@ -1,6 +1,6 @@
-const peopleUrl = "https://swapi.dev/api/people/"
-const vehicleUrl = "https://swapi.dev/api/vehicles/"
-const planetsUrl = "https://swapi.dev/api/planets/"
+// const peopleUrl = "https://swapi.dev/api/people/"
+// const vehicleUrl = "https://swapi.dev/api/vehicles/"
+// const planetsUrl = "https://swapi.dev/api/planets/"
 const urlObject = {
     "people": "https://swapi.dev/api/people/",
     "vehicle": "https://swapi.dev/api/vehicles/",
