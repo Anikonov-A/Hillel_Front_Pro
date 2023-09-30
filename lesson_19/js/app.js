2bf314691aaf337535a83412196759bf2a966450
Anton Anikonov <antonanikonov1@gmail.com>
2023-10-01 00:21:22 +0300

    add feautures

--- lesson_19/js/app.js
+++ lesson_19/js/app.js
@@ -1,5 +1,6 @@
 
+// getAndDisplayData()
 
-getAndDisplayPeopleData();
-getAndDisplayVehicleData();
-getAndDisplayPlanetsData();
+getAndDisplayData(domElements.peopleBtn, domElements.peopleNextBtn, domElements.peoplePrevBtn, currentPeoplePage,functionsList.peopleFunc);
+getAndDisplayData(domElements.vehicleBtn, domElements.vehicleNextBtn, domElements.vehiclePrevBtn, currentVehiclesPage,functionsList.vehicleFunc);
+getAndDisplayData(domElements.planetsBtn, domElements.planetsNextBtn, domElements.planetsPrevBtn, currentPlanetsPage,functionsList.planetsFunc);
--- lesson_19/js/function.js
+++ lesson_19/js/function.js
@@ -1,98 +1,148 @@
-function getAndDisplayPeopleData() {
-    const {peopleBtn, peopleList, peopleNextBtn, peopleBlock, modalTitle, modalList} = domElements;
-    peopleBtn.addEventListener('click', () => {
-        axios.get(urlObject.people)
-            .then(people => {
-                const results = people.data.results;
-                peopleList.innerHTML = '';
-                if (people.data.next !== null) {
-                    peopleNextBtn.style.visibility = 'visible'
-                }
-                results.forEach(people => {
-                    peopleBlock.style.visibility = 'visible'
-                    createElement(`li`, '#peopleList', people.name, {
-                        'data-bs-toggle': "modal",
-                        'data-bs-target': "#myModal"
+let currentPeoplePage= 1;
+let currentVehiclesPage= 1;
+let currentPlanetsPage= 1;
+
+
+function getAndDisplayData(button,nextBtn,prevBtn,currentPage,callback) {
+    button.addEventListener('click', () => {
+        callback(currentPage);
+    });
+    nextBtn.addEventListener('click', () => {
+        currentPage++;
+        callback(currentPage)
+    });
+    prevBtn.addEventListener('click', () => {
+        currentPage--;
+        callback(currentPage);
+    });
+}
+// function getAndDisplayData() {
+//     const {peoplePrevBtn, peopleNextBtn, peopleBtn, vehiclePrevBtn, vehicleNextBtn, vehicleBtn, planetsPrevBtn, planetsNextBtn, planetsBtn} = domElements;
+//
+//     peopleBtn.addEventListener('click', () => {
+//         getAndDisplayPeopleData(currentPeoplePage);
+//     });
+//     peopleNextBtn.addEventListener('click', () => {
+//         currentPeoplePage++;
+//         getAndDisplayPeopleData(currentPeoplePage)
+//     });
+//     peoplePrevBtn.addEventListener('click', () => {
+//         currentPeoplePage--;
+//         getAndDisplayPeopleData(currentPeoplePage);
+//     });
+//     vehicleBtn.addEventListener('click', () => {
+//         getAndDisplayVehicleData(currentVehiclesPage);
+//     });
+//     vehicleNextBtn.addEventListener('click', () => {
+//         currentVehiclesPage++;
+//         getAndDisplayVehicleData(currentVehiclesPage);
+//     });
+//     vehiclePrevBtn.addEventListener('click', () => {
+//         currentVehiclesPage--;
+//         getAndDisplayVehicleData(currentVehiclesPage)
+//     });
+//     planetsBtn.addEventListener('click', () => {
+//         getAndDisplayPlanetsData(currentPlanetsPage)
+//     });
+//     planetsNextBtn.addEventListener('click',()=>{
+//         currentPlanetsPage++;
+//         getAndDisplayPlanetsData(currentPlanetsPage)
+//     });
+//     planetsPrevBtn.addEventListener('click',()=>{
+//         currentPlanetsPage--;
+//         getAndDisplayPlanetsData(currentPlanetsPage);
+//     });
+// }
+
+function getAndDisplayPeopleData(currentPage) {
+    const {peopleList, peopleNextBtn, peoplePrevBtn, peopleBlock, modalTitle, modalList} = domElements;
+    axios.get(urlObject.people + `?page=${currentPage}`)
+        .then(people => {
+            const results = people.data.results;
+            peopleList.innerHTML = '';
+            people.data.next !== null ? peopleNextBtn.style.visibility = 'visible' : peopleNextBtn.style.visibility = 'hidden';
+            people.data.previous !== null ? peoplePrevBtn.style.visibility = 'visible' : peoplePrevBtn.style.visibility = 'hidden';
+
+            results.forEach(people => {
+                peopleBlock.style.visibility = 'visible'
+                createElement(`li`, '#peopleList', people.name, {
+                    'data-bs-toggle': "modal",
+                    'data-bs-target': "#myModal"
+                })
+            });
+            peopleBlock.addEventListener('click', (event) => {
+                if (event.target.nodeName === 'LI') {
+                    const listItemText = event.target.textContent;
+                    const character = results.find(person => person.name === listItemText);
+
+                    modalTitle.textContent = character.name;
+                    modalList.innerHTML = '';
+                    dataProperties.peopleProperties.forEach(property => {
+                        createElement(`li`, '#modalList', `${property}: ${character[property]}`);
                     })
+                }
+            })
+        })
+}
+
+function getAndDisplayVehicleData(currentPage) {
+    const {vehicleList, vehicleNextBtn, vehiclePrevBtn, vehicleBlock, modalTitle, modalList} = domElements;
+    axios.get(urlObject.vehicle + `?page=${currentPage}`)
+        .then(vehicles => {
+            const results = vehicles.data.results;
+            vehicleList.innerHTML = '';
+            vehicles.data.next !== null ? vehicleNextBtn.style.visibility = 'visible' : vehicleNextBtn.style.visibility = 'hidden';
+            vehicles.data.previous !== null ? vehiclePrevBtn.style.visibility = 'visible' : vehiclePrevBtn.style.visibility = 'hidden';
+            results.forEach(vehicle => {
+                vehicleBlock.style.visibility = 'visible'
+                createElement(`li`, '#vehicleList', vehicle.name, {
+                    'data-bs-toggle': "modal",
+                    'data-bs-target': "#myModal"
                 });
-                peopleBlock.addEventListener('click', (event) => {
-                    if (event.target.nodeName === 'LI') {
-                        const listItemText = event.target.textContent;
-                        const character = results.find(person => person.name === listItemText);
-
-                        modalTitle.textContent = character.name;
-                        modalList.innerHTML = '';
-                        dataProperties.peopleProperties.forEach(property => {
-                            createElement(`li`, '#modalList', `${property}: ${character[property]}`);
-                        })
-                    }
-                })
             })
-    })
+            vehicleBlock.addEventListener('click', (event) => {
+                if (event.target.nodeName === 'LI') {
+                    const listItemText = event.target.textContent;
+                    const vehicles = results.find(vehicle => vehicle.name === listItemText);
+
+                    modalTitle.textContent = vehicles.name;
+                    modalList.innerHTML = '';
+                    dataProperties.vehicleProperties.forEach(property => {
+                        createElement(`li`, '#modalList', `${property}: ${vehicles[property]}`);
+                    })
+                }
+            })
+        })
+
 }
 
-function getAndDisplayVehicleData() {
-    const {vehicleBtn, vehicleList, vehicleNextBtn, vehicleBlock, modalTitle, modalList} = domElements;
-    vehicleBtn.addEventListener('click', () => {
-        axios.get(urlObject.vehicle)
-            .then(vehicles => {
-                const results = vehicles.data.results;
-                vehicleList.innerHTML = '';
-                if (vehicles.data.next !== null) {
-                    vehicleNextBtn.style.visibility = 'visible'
-                }
-                results.forEach(vehicle => {
-                    vehicleBlock.style.visibility = 'visible'
-                    createElement(`li`, '#vehicleList', vehicle.name, {
-                        'data-bs-toggle': "modal",
-                        'data-bs-target': "#myModal"
-                    });
-                })
-                vehicleBlock.addEventListener('click', (event) => {
-                    if (event.target.nodeName === 'LI') {
-                        const listItemText = event.target.textContent;
-                        const vehicles = results.find(vehicle => vehicle.name === listItemText);
+function getAndDisplayPlanetsData(currentPage) {
+    const { planetsList, planetsNextBtn,planetsPrevBtn, planetsBlock, modalTitle, modalList} = domElements
+    axios.get(urlObject.planets + `?page=${currentPage}`)
+        .then(planets => {
+            const results = planets.data.results;
+            planetsList.innerHTML = '';
+            planets.data.next !== null ? planetsNextBtn.style.visibility = 'visible': planetsNextBtn.style.visibility = 'hidden';
+            planets.data.previous !== null ? planetsPrevBtn.style.visibility = 'visible': planetsPrevBtn.style.visibility = 'hidden';
 
-                        modalTitle.textContent = vehicles.name;
-                        modalList.innerHTML = '';
-                        dataProperties.vehicleProperties.forEach(property => {
-                            createElement(`li`, '#modalList', `${property}: ${vehicles[property]}`);
-                        })
-                    }
-                })
+            results.forEach(planet => {
+                planetsBlock.style.visibility = 'visible'
+                createElement(`li`, '#planetsList', planet.name, {
+                    'data-bs-toggle': "modal",
+                    'data-bs-target': "#myModal"
+                });
             })
-    })
-}
+            document.getElementById('planetsBlock').addEventListener('click', (event) => {
+                if (event.target.nodeName === 'LI') {
+                    const listItemText = event.target.textContent;
+                    const planets = results.find(planet => planet.name === listItemText)
 
-function getAndDisplayPlanetsData() {
-    const {planetsBtn, planetsList, planetsNextBtn, planetsBlock, modalTitle, modalList} = domElements
-    planetsBtn.addEventListener('click', () => {
-        axios.get(urlObject.planets)
-            .then(planets => {
-                const results = planets.data.results;
-                planetsList.innerHTML = '';
-                if (planets.data.next !== null) {
-                    planetsNextBtn.style.visibility = 'visible';
+                    modalTitle.textContent = planets.name;
+                    modalList.innerHTML = '';
+                    dataProperties.planetsProperties.forEach(property => {
+                        createElement(`li`, '#modalList', `${property}: ${planets[property]}`);
+                    })
                 }
-                results.forEach(planet => {
-                    planetsBlock.style.visibility = 'visible'
-                    createElement(`li`, '#planetsList', planet.name, {
-                        'data-bs-toggle': "modal",
-                        'data-bs-target': "#myModal"
-                    });
-                })
-                document.getElementById('planetsBlock').addEventListener('click', (event) => {
-                    if (event.target.nodeName === 'LI') {
-                        const listItemText = event.target.textContent;
-                        const planets = results.find(planet => planet.name === listItemText)
-
-                        modalTitle.textContent = planets.name;
-                        modalList.innerHTML = '';
-                        planetsProperties.forEach(property => {
-                            createElement(`li`, '#modalList', `${property}: ${planets[property]}`);
-                        })
-                    }
-                })
             })
-    })
+        })
 }
