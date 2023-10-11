a21e28b1367edbf2b7838af01cdcb990da59105f
Anton Anikonov <antonanikonov1@gmail.com>
2023-10-11 13:16:41 +0300

    add class DomElement

---
+++ lesson_22/js/DomElement.js
@@ -0,0 +1,50 @@
+class CreateDomElement {
+    constructor({element, attributes, content, parentElement, handlers}) {
+        this.element = this.createElement(element);
+        this.setAttributes(attributes);
+        this.setContent(content)
+        this.appendTo(parentElement)
+        this.setHandler(handlers)
+
+    }
+
+    createElement = function (element) {
+        return document.createElement(element)
+    }
+    setAttributes = function (attributes) {
+        attributes.forEach(attributeObj => {
+            for (let key in attributeObj) {
+                if (key === 'className') {
+                    this.element.setAttribute('class', attributeObj[key]);
+                } else {
+                    this.element.setAttribute(key, attributeObj[key]);
+                }
+            }
+        })
+    }
+    setContent = function (content) {
+        this.element.textContent = content;
+    }
+    appendTo = function (parentElement) {
+        const parent = typeof parentElement === 'string' ? document.querySelector(parentElement) : parentElement;
+        parent.appendChild(this.element);
+    }
+
+    setHandler = function (handlers) {
+        for (let eventName in handlers) {
+            this.element.addEventListener(eventName, handlers[eventName]);
+        }
+    }
+}
+const liParam ={
+    element:'li',
+    attributes:[{className: 'list-item'}],
+    content: 'list-item',
+    parentElement:`#list`,
+    handlers:{'click': () => console.log('click')}
+}
+
+const li = new CreateDomElement(liParam)
+
+
+
