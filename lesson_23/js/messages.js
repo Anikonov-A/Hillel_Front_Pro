d08a70fe8dc9f9e6a2a4d0ced10a98a9711d8117
Anton Anikonov <antonanikonov1@gmail.com>
2023-10-14 20:27:56 +0300

    add features

---
+++ lesson_23/js/messages.js
@@ -0,0 +1,23 @@
+const answers =[
+ `Hello human`,
+ `What do you want to know ?`,
+ `I need to think...`,
+ `How do you feel about cows?`,
+ `That’s a question I can’t answer`,
+ `You know I’m just a machine`,
+ `Why are you asking me this? You don’t know how to use Google?`,
+ `Blah blah, boring`,
+ `I don’t get paid to talk to you`,
+ `Bye bye human`,
+]
+const jokes=[
+ "What did one plate say to the other plate? 'Lunch is on me!'",
+ "I'm on a seafood diet. I see food, and I eat it.",
+ "Why don't scientists trust atoms? Because they make up everything!",
+ "I told my computer I needed a break, and now it won't stop sending me Kit-Kat memes.",
+ "Why did the math book look sad? Because it had too many problems.",
+ "What do you call a bear with no teeth? A gummy bear!",
+ "Why don't oysters donate to charity? Because they are shellfish.",
+ "What's a JavaScript developer's favorite soda? Callback-ula!",
+ "Why do JavaScript developers make terrible DJs? Because they don't know when to 'break' the loop.",
+]
