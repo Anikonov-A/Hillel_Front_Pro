a13ba80bf87351ed295d36f5692d6eb419f8564a
Anton Anikonov <antonanikonov1@gmail.com>
2023-11-07 13:54:25 +0200

    First commit

---
+++ lesson_29_react/src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import './App.module.scss';
+import Header from '../header/Header'
+import Page from '../page/Page'
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Page />
+        </div>
+    );
+}
+
+export default App;
